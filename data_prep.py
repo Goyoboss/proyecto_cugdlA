abc333ee7ed1d4896057af774652697c3c904fc1
Maria sanchez <maria.sanchez6150@alumnos.udg.mx>
2026-04-23 17:59:53 -0600

    dataframe anadidos

--- data_prep.py
+++ data_prep.py
@@ -1,17 +1,19 @@
 import pandas as pd
-from data import url_ps
-from data import url_dr
+from data import url_ps, url_dr, url_stan
 
+import ssl
+#Desactivar la verificacion de seguridad SSL
+ssl.create_default_https_context = ssl._create_unverified_context
 
 df_feo_ps = pd.read_csv(url_ps)
 df_feo_dr = pd.read_csv(url_dr)
+df_feo_stan = pd.read_csv(url_stan)
 
 #---Limpieza de datos---#
 
 # se eliminan columnas innecesarias y redundantes
 
-def df_ps():
-    return (df_feo_ps
+df_ps=(df_feo_ps
     .drop(columns=['time', 'duration']) #elimina columnas
     .loc[lambda df: df['stop']<7] # quita valores mayores a 7
     .groupby(['raceId', 'driverId']) #agrupa conductores y carreras
@@ -24,11 +26,15 @@ def df_ps():
     .reset_index() 
     )
 
-def df_dr():
-    return (df_feo_dr
-    .drop(columns=['url', 'number', 'code', 'surname']) #elimina columnas
+df_dr=(df_feo_dr
+    .drop(columns=['url', 'number', 'code', 'surname', 'dob']) #elimina columnas
     #viendo valores unicos argentina y argentino se repiten, entonces se reemplazan.
     .assign(nationality=lambda df:df['nationality'].replace('Argentine', 'Argentinian '))
+    )
+
+df_stan=(df_feo_stan
+    .drop(columns=['driverStandingsId', 'positionText', 'points', 'wins']) #Quitar columnas innecesatias
+    .loc[lambda df:df['position']<11] #quitar posiciones mayores a 11
     )
 
 '''
@@ -37,5 +43,26 @@ con el driverId y se agregue en una nueva columna correspondiente con su
 Id de conductor
 '''
 
-#---Data wrangling---#
+df_ps_dr = df_ps.merge(df_dr, on='driverId', how='left') #Combina Pitstop y Drivers (PsDr)
+df_psdrstan = df_ps_dr.merge(df_stan, on=['raceId', 'driverId'], how='left') 
+df_psdrstan = df_psdrstan.dropna().reset_index() #Combina PsDr y standings
 
+print(f'---PitStops, Drivers y Standings unidos---\n')
+print(f'{df_psdrstan.head()}\n') 
+
+#Aseguramos que no hay ningun valor nulo (copiado de Iván Toledano😢)
+null_counts = df_psdrstan.isnull().sum()
+null_percent = (null_counts / len(df_psdrstan)) * 100
+
+missing_data = pd.DataFrame({
+    'Valores nulos': null_counts,
+    '% del total': null_percent.round(2)
+})
+print('---Valores nulos---\n')
+print(f'{missing_data} \n')
+
+#Con los datos limpios se sacan las carreras que ha hecho Hamilton
+def df_raikk():
+    return df_psdrstan[df_psdrstan['driverRef']=='raikkonen']
+
+#---Data wrangling---
